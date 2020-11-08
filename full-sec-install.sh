c610aa9212fe65ec99a5781c2a03e4ce74ba9057
n0nuser <pjgr2000@gmail.com>
2020-11-08 10:30:20 +0100

    Update

--- full-sec-install.sh
+++ full-sec-install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # Author: n0nuser ~ https://github.com/n0nuser
 
-location="/opt"
+
+location="/opt" # Location where all the tools are going to be downloaded
+downloads="~/Descargas" # Put here your downloads folder from ~
 
 ## APT-Manager
+cd ~ && rm -rf .bash_aliases && wget https://gist.github.com/n0nuser/34fc14a084436ae89c2b3405ad453f0a/raw/e3f4d4be8bc13bd532b2d396846bf4cdd9885fee/.bash_aliases -O .bash_aliases && source ~/.bash_aliases
+update
+### General
 sudo apt-get install -y git wget python python3 pip pip3
 ### For rsa-tool
 sudo apt-get install -y libgmp3-dev libmpc-dev
 ### For Krackattacks
 sudo apt-get install -y libnl-3-dev libnl-genl-3-dev pkg-config libssl-dev net-tools git sysfsutils python-scapy python-pycryptodome virtualenv
+
+## Download VSCode
+cd $downloads && wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb && sudo apt-get install ./vscode.deb -y
 
 ## Backdoors
 cd $location && mkdir Backdoor && cd Backdoor
