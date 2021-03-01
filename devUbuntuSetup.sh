eb8d6d52fc5d4979cea9e0f3b5065b5d072abc79
n0nuser <pjgr2000@gmail.com>
2021-03-01 13:23:22 +0100

    Update

--- devUbuntuSetup.sh
+++ devUbuntuSetup.sh
@@ -2,8 +2,16 @@
 
 sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get dist-upgrade -y && sudo apt autoremove -y
 
-sudo apt install -y hugo git tilix gcc python3
+sudo apt install -y git tilix gcc python3
 
 sudo snap install --classic code
+
+sudo snap install hugo
+
+echo -e "\n\n\e[33mHugo Version:\e[0m"
+
+hugo version
+
+echo -e "\n\n"
 
 cd ~ && git clone https://github.com/n0nuser/blog
